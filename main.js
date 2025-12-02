ad71fee68d28375d86b4d7ef883f7448bcfe6cc2
rohit vidhate <rohitvidhate3@gmail.com>
2025-12-02 23:30:32 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,64 @@
+
+function toggleMenu(){
+  const menu = document.getElementById('navbar');
+  menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
+}
+
+/*===== MENU SHOW =====*/ 
+
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*==================== REMOVE MENU MOBILE ====================*/
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 200,
+});
+
+sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text ,.nav',{}); 
+sr.reveal('.about__subtitle, .about__text, .skills__img, .btn1, .container, .blob-box, .about-details,.hobby',{delay: 300}); 
+sr.reveal('.home__social-icon',{ interval: 200}); 
+sr.reveal('.skills__data, .work__img, .project,.contact__input',{interval: 200});
