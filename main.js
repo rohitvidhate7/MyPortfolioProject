f22e90a247260656afd5a278fc7f1b0acfb91361
Rohit Sandip Vidhate <138186608+rohitvidhate7@users.noreply.github.com>
2025-12-13 20:32:18 +0530

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId), 
     nav = document.getElementById(navId)
   
-    if(toggle && nav){
+    if(toggle && nav){ 
         toggle.addEventListener('click', ()=>{
             nav.classList.toggle('show') 
         }) 
