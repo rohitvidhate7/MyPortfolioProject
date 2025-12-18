c20bcfe820e8367e455c553962e097d964f856ab
Rohit Sandip Vidhate <138186608+rohitvidhate7@users.noreply.github.com>
2025-12-18 19:10:16 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function toggleMenu(){
 }
 
 /*===== MENU SHOW =====*/  
-
+ 
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId), 
     nav = document.getElementById(navId)
